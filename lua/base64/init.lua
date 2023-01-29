ee75098cd8a855e2f11a94c1c966a94d35b8b7f5
Mikael Elkiaer <2102306+MikaelElkiaer@users.noreply.github.com>
2023-01-29 13:34:33 +0100

    Create encode popup maps

--- lua/base64/init.lua
+++ lua/base64/init.lua
@@ -4,8 +4,8 @@ M.encode = function()
 	local row2, col2 = unpack(vim.api.nvim_win_get_cursor(0))
 	local content = vim.api.nvim_buf_get_text(0, row1 - 1, col1 - 1, row2 - 1, col2 + 1, {})
 	local text = table.concat(content, "\n")
-	local decoded = vim.fn.system({ "base64", "--wrap", "0" }, text)
-	decoded = string.gsub(decoded, "%s+", "")
+	local encoded = vim.fn.system({ "base64", "--wrap", "0" }, text)
+	encoded = string.gsub(encoded, "%s+", "")
 
 	local Popup = require("nui.popup")
 	local popup = Popup({
@@ -25,8 +25,13 @@ M.encode = function()
 			height = 5,
 		},
 	})
+  popup:map("n", "q", vim.api.nvim_buf_delete)
+  popup:map("n", "w", function(bufnr)
+    vim.api.nvim_buf_set_text(vim.api.nvim_get_current_buf(), row1 - 1, col1 - 1, row2 - 1, col2 - 1, { encoded })
+    vim.api.nvim_buf_delete(bufnr, {})
+  end)
 	popup:mount()
-	vim.api.nvim_buf_set_lines(popup.bufnr, 0, 1, false, { decoded })
+	vim.api.nvim_buf_set_lines(popup.bufnr, 0, 1, false, { encoded })
 end
 
 return M
